5bc59c0378fb1ff52f0b897d4913d5812f663dcc
deyris253 <d_razafindrakoto1@techandcodefactory.fr>
2021-02-05 18:07:27 +0100

    Login Front-End

--- app/src/main/java/com/example/hermes/Home.java
+++ app/src/main/java/com/example/hermes/Home.java
@@ -2,83 +2,13 @@ package com.example.hermes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.text.TextUtils;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ProgressBar;
-import android.widget.TextView;
-
-import com.google.firebase.auth.FirebaseAuth;
 
 public class Home extends AppCompatActivity {
-
-    private FirebaseAuth vFirebaseAuth;
-    private EditText vEmail;
-    private EditText vPassword;
-    private Button vLoginButton;
-
-    private Button vCreateButton;
-    private Button vPasswordForgottenButton;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-
-        vFirebaseAuth = FirebaseAuth.getInstance();
-        vEmail = findViewById(R.id.typeEmail);
-        vPassword = findViewById(R.id.typePassword);
-        vLoginButton = findViewById(R.id.loginButton);
-        vCreateButton = findViewById(R.id.createAccount);
-        vPasswordForgottenButton = findViewById(R.id.passwordForgotten);
-
-
-        vLoginButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String sEmail = vEmail.getText().toString().trim();
-                String sPassword = vPassword.getText().toString().trim();
-
-                if (TextUtils.isEmpty(sEmail)) {
-                    vEmail.setError("Veuillez indiquer votre email.");
-                }
-
-                if (TextUtils.isEmpty(sPassword)) {
-                    vPassword.setError("Veuillez indiquer votre mot de passe.");
-                }
-
-                if (sPassword.length() < 7) {
-                    vPassword.setError("Votre mot de passe doit contenir au moins 7 caractères.");
-                }
-            }
-        });
-
-             /* vFirebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                 @Override
-                 public void onComplete(@NonNull Task<AuthResult> task) {
-
-                 }
-             }) */
-
-
-        vCreateButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(), CreateAccount.class));
-            }
-        });
-
-        vPasswordForgottenButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(), PasswordForgotten.class));
-            }
-        });
-
     }
-
 }
