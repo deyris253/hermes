d7df0907cbda6b4bd94ff602422b9c9d07ec3e07
deyris253 <d_razafindrakoto1@techandcodefactory.fr>
2021-02-09 20:35:05 +0100

    Facebook Button

--- app/src/main/java/com/example/hermes/FacebookActivity.java
+++ app/src/main/java/com/example/hermes/FacebookActivity.java
@@ -2,14 +2,13 @@ package com.example.hermes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 
-public class Home extends AppCompatActivity {
+public class FacebookActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_facebook);
     }
 }
--- app/src/main/java/com/example/hermes/Login.java
+++ app/src/main/java/com/example/hermes/Login.java
@@ -7,19 +7,34 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.firebase.auth.FirebaseAuth;
 
+import org.w3c.dom.Text;
+
+import java.util.Arrays;
+
 public class Login extends AppCompatActivity implements View.OnClickListener {
 
-    private static final int RC_SIGN_IN = 7544;
     private EditText vEmail;
     private EditText vPassword;
-    private Button vLoginButton;
+    private TextView vLoginChoice;
     private FirebaseAuth vFirebaseAuth;
+
+    CallbackManager callbackManager;
+
+    private static final int RC_SIGN_IN = 7544;
 
     private GoogleSignInClient vGoogleSignClient;
 
@@ -35,12 +50,39 @@ public class Login extends AppCompatActivity implements View.OnClickListener {
         vFirebaseAuth = FirebaseAuth.getInstance();
         vEmail = findViewById(R.id.typeEmail);
         vPassword = findViewById(R.id.typePassword);
-        vLoginButton = findViewById(R.id.loginButton);
+        vLoginChoice = findViewById(R.id.loginChoice);
         vCreateButton = findViewById(R.id.createAccount);
         vPasswordForgottenButton = findViewById(R.id.passwordForgotten);
 
-        findViewById(R.id.googleButton).setOnClickListener(this);
 
+        callbackManager = CallbackManager.Factory.create();
+
+
+        LoginManager.getInstance().registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
+            }
+        });
+
+        ImageButton fbButton = findViewById(R.id.facebookButton);
+
+        fbButton.setOnClickListener(v -> LoginManager.getInstance().logInWithReadPermissions(this,
+                Arrays.asList("public_profile", "user_friends")));
+
+
+
+        findViewById(R.id.googleButton).setOnClickListener(this);
 
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestEmail()
@@ -54,6 +96,12 @@ public class Login extends AppCompatActivity implements View.OnClickListener {
 
         vPasswordForgottenButton.setOnClickListener(v -> startActivity(new Intent(getApplication(), PasswordForgotten.class)));
 
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 
     @Override
